f1de0b229b3327b7d6d9ce1d0627dc779a8a51e2
adamwhitneysoftwire <adam.whitney@softwire.com>
2021-11-17 10:58:41 +0000

    Part 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,31 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                string Output = "";
+                
+                // Check multiple of 3
+                if (i % 3 == 0)
+                {
+                    Output = Output + "Fizz";
+                }
+                
+                // Check multiple of 5
+                if (i % 5 == 0)
+                {
+                    Output = Output + "Buzz";
+                }
+
+                // If no output text, output the number
+                if (Output.Length == 0)
+                {
+                    Output = i.ToString();
+                }
+                
+                // Write output to the console
+                Console.Out.WriteLine(Output);
+            }
         }
     }
 }
