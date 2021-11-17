93257ae5ff7af234b0d069647be70af3f8f52e01
adamwhitneysoftwire <adam.whitney@softwire.com>
2021-11-17 15:20:39 +0000

    Part 2 complete

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzz
 {
@@ -6,47 +7,48 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 200; i++)
+            for (int i = 1; i <= 300; i++)
             {
-                string output = "";
+                List<string> output = new List<string>();
+                
+                if (i % 3 == 0 & i % 11 > 0)
+                {
+                    output.Add("Fizz");
+                }
+                
+                if (i % 13 == 0)
+                {
+                    output.Add("Fezz");
+                }
 
                 if (i % 11 == 0)
                 {
-                    if (i % 13 == 0)
-                    {
-                        output += "Fezz";
-                    }
-                    output += "Bong";
+                    output.Add("Bong");
                 }
                 else
                 {
-                    if (i % 3 == 0)
-                    {
-                        output += "Fizz";
-                    }
-                    
-                    if (i % 13 == 0)
-                    {
-                        output += "Fezz";
-                    }
-                    
                     if (i % 5 == 0)
                     {
-                        output += "Buzz";
-                    }
-                    
-                    if (i % 7 == 0)
-                    {
-                        output += "Bang";
+                        output.Add("Buzz");
                     }
 
-                    if (output.Length == 0)
+                    if (i % 7 == 0)
                     {
-                        output = i.ToString();
+                        output.Add("Bang");
+                    }
+                    
+                    if (output.Count == 0)
+                    {
+                        output.Add(i.ToString());
                     }
                 }
+
+                if (i % 17 == 0)
+                {
+                    output.Reverse();
+                }
                 
-                Console.Out.WriteLine(output);
+                Console.Out.WriteLine(String.Join("", output));
             }
         }
     }
