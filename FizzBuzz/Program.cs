d85f1b273f7339f96b8d8726f3e1e61daf1786a1
adamwhitneysoftwire <adam.whitney@softwire.com>
2021-11-17 11:16:42 +0000

    Converting to compound operators and lowercase variable

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,28 +8,28 @@ namespace FizzBuzz
         {
             for (int i = 1; i <= 100; i++)
             {
-                string Output = "";
+                string output = "";
                 
                 // Check multiple of 3
                 if (i % 3 == 0)
                 {
-                    Output = Output + "Fizz";
+                    output += "Fizz";
                 }
                 
                 // Check multiple of 5
                 if (i % 5 == 0)
                 {
-                    Output = Output + "Buzz";
+                    output += "Buzz";
                 }
 
                 // If no output text, output the number
-                if (Output.Length == 0)
+                if (output.Length == 0)
                 {
-                    Output = i.ToString();
+                    output = i.ToString();
                 }
                 
                 // Write output to the console
-                Console.Out.WriteLine(Output);
+                Console.Out.WriteLine(output);
             }
         }
     }
